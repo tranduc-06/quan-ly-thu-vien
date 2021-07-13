d77dbe7bcc5da69c1b4892de2bd8b43741434ad4
ThangVu162 <56299002+ThangVu162@users.noreply.github.com>
2021-07-14 00:11:31 +0700

    code

---
+++ fontend/js/slideshow.js
@@ -0,0 +1,26 @@
+var slideIndex = 1;
+        showSlides(slideIndex);
+        
+        function plusSlides(n) {
+          showSlides(slideIndex += n);
+        }
+        
+        function currentSlide(n) {
+          showSlides(slideIndex = n);
+        }
+        
+        function showSlides(n) {
+          var i;
+          var slides = document.getElementsByClassName("mySlides");
+          var image = document.getElementsByClassName("image");
+          if (n > slides.length) {slideIndex = 1}    
+          if (n < 1) {slideIndex = slides.length}
+          for (i = 0; i < slides.length; i++) {
+              slides[i].style.display = "none";  
+          }
+          for (i = 0; i < image.length; i++) {
+            image[i].className = image[i].className.replace(" myactive", "");
+          }
+          slides[slideIndex-1].style.display = "block";  
+          image[slideIndex-1].className += " myactive";
+        }
